8714b48c354fa21525723f1f796e9b11d291d153
imasha85 <imasha@yandex.ru>
2017-09-03 16:43:43 +0300

    Update geoxml_display.js

--- geoxml_display.js
+++ geoxml_display.js
@@ -49,7 +49,7 @@ function init () {
         e.target.disabled = true;
     });
     kmlButton.click(function (e) {
-        ymaps.geoXml.load('geoObjects.kml')
+        ymaps.geoXml.load('ZTL Italia.kml')
             .then(onGeoXmlLoad);
         e.target.disabled = true;
     });
