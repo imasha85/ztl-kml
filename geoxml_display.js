be0a50dcfbd6878257af1cbc3f698b2f166cccad
imasha85 <imasha@yandex.ru>
2017-09-03 17:31:05 +0300

    Update geoxml_display.js

--- geoxml_display.js
+++ geoxml_display.js
@@ -8,7 +8,9 @@ function init () {
             searchControlProvider: 'yandex#search'
         }),
         //--> из kml
+                gpxButton = $('.load-gpx'),
         kmlButton = $('.load-kml'),
+        kmlFlightsButton = $('.load-kml-flights'),
         //<-- из kml
         objectManager = new ymaps.ObjectManager({
             // Чтобы метки начали кластеризоваться, выставляем опцию.
@@ -34,17 +36,38 @@ function init () {
         //--> из kml
 
     // Отключение кеширования атрибута disabled в Firefox.
+    gpxButton.get(0).disabled = false;
     kmlButton.get(0).disabled = false;
+    kmlFlightsButton.get(0).disabled = false;
     
     
         // При нажатии на кнопку загружаем соответствующий XML-файл
     // и отображаем его данные на карте.
+    gpxButton.click(function (e) {
+        ymaps.geoXml.load('geoObjects.gpx')
+            .then(onGeoXmlLoad);
+        e.target.disabled = true;
+    });
     kmlButton.click(function (e) {
         ymaps.geoXml.load('ZTL.kml')
             .then(onGeoXmlLoad);
         e.target.disabled = true;
     });
-
+    kmlFlightsButton.click(function (e) {
+        ymaps.geoXml.load('http://openflights.org/demo/openflights-sample.kml')
+            .then(function (res) {
+                res.geoObjects.each(function (obj) {
+                    obj.options.set({preset: 'islands#blackCircleIcon'});
+                    if (!obj.geometry) {
+                        obj.each(function (obj) {
+                            obj.options.set({strokeColor: "9090e8"});
+                        });
+                    }
+                });
+                onGeoXmlLoad(res);
+            });
+        e.target.disabled = true;
+    });
 
     // Обработчик загрузки XML-файлов.
     function onGeoXmlLoad(res) {
@@ -57,7 +80,6 @@ function init () {
         }
     }
           //<-- из kml
-  
     
 
 }
